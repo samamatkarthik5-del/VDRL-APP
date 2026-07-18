803ccd8c9872d7cad195742532d4b50ade101552
Karthik <samamatkarthik5@gmail.com>
2026-07-18 21:09:23 +0400

    urls fix

--- vdrl_project/urls.py
+++ vdrl_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(
@@ -22,3 +23,8 @@ urlpatterns = [
         ),
     ),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
